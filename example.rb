ae4ff8b3e1976839f2a795534edf5b807ab25125
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2019-05-04 01:11:38 +0100

    Update example.rb

--- example.rb
+++ example.rb
@@ -47,4 +47,34 @@ end
 
 task default: :run
 
-# this is too funny (https://www.youtube.com/watch?v=datDkio1AXM) but I'm serious, ruby can be a better JS
+
+
+# 
+
+### Functions
+
+def foo
+  "bar"
+end
+
+foo = -> { "bar" }
+
+# or if you want to resemble JS:
+
+foo = -> { return "bar" }
+
+# TODO: add the others
+
+# This is good but I haven't tried it:
+
+https://medium.com/@teabass/emulating-the-node-js-module-system-in-ruby-d84a5289d30e
+
+
+# Testing library: RSpec 
+
+Rspec.describe("app") {
+  specify("this is a test") {
+    test = Test.().foo()
+    test.should === true
+  }
+}
